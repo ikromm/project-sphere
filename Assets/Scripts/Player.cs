8cb640ec155c5f8a86f8b4414a7539be0b8250eb
ikromm <jkrommlar@yahoo.gr>
2016-04-05 13:22:33 +0300

    initial commit, player movement implemented for horizontal axis

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Deltahacker
+{
+    public class Player : MonoBehaviour
+    {
+        public float Speed = 3f;
+
+        public void Update()
+        {
+            float horizontal = Input.GetAxis("Horizontal");
+
+            Vector3 movement = new Vector3(horizontal * Speed * Time.deltaTime, 0, 0);
+            transform.position += movement;
+        }
+    }
+}
