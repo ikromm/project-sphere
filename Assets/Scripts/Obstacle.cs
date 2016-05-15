0eb4fbbe69aa38b4beaa34265107d3db442ac224
ikromm <jkrommlar@yahoo.gr>
2016-05-15 13:51:23 +0300

    added obstacles and obstacle spawner scripts and prefabs

---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace Deltahacker
+{
+    public class Obstacle : MonoBehaviour
+    {
+        public float WidthModifier = 1;
+        public float Speed;
+
+        public void Start()
+        {
+            transform.localScale = new Vector3(WidthModifier, 1, 1);
+        }
+
+        public void Update()
+        {
+            transform.position += new Vector3(Speed * Time.deltaTime, 0, 0);
+        }
+
+        public static GameObject Instantiate(InitializationData data, float scale, float speed)
+        {
+            GameObject instance = (GameObject)Instantiate(data.Prefab, data.Position, data.Rotation);
+
+            Obstacle script = instance.GetComponent<Obstacle>();
+            script.Speed = speed;
+            script.WidthModifier = scale;
+
+            return instance;
+        }
+    }
+
+    public struct InitializationData
+    {
+        public GameObject Prefab;
+        public Vector3 Position;
+        public Quaternion Rotation;
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+namespace Deltahacker
+{
+    public class Spawner : MonoBehaviour
+    {
+        public GameObject ObstaclePrefab;
+        public float Speed;
+
+        public float Frequency;
+        public FloatRange WidthModifier;
+
+        private float lastSpawn;
+
+        public void Update()
+        {
+            if (Time.time >= lastSpawn + Frequency)
+            {
+                lastSpawn = Time.time;
+                spawnObstacle();
+            }
+        }
+
+        private void spawnObstacle()
+        {
+            Obstacle.Instantiate(new InitializationData()
+            {
+                Position = transform.position,
+                Rotation = transform.rotation,
+                Prefab = ObstaclePrefab
+            }, WidthModifier.Random(), Speed);
+        }
+    }
+
+
+
+    [System.Serializable]
+    public struct FloatRange
+    {
+        public float Min;
+        public float Max;
+
+        public float Random()
+        {
+            return UnityEngine.Random.Range(Min, Max);
+        }
+    }
+}
